e3c59ec0dff8a3ff1e4cd3452f4d2f372d3f9ee0
RioShiina47 <RioShiina47@outlook.com>
2026-03-30 15:27:11 +0800

    [ImageEdit] Add FireRed-Image-Edit module.

---
+++ frontend/module/image_edit/instruction_edit/firered_image_edit/firered_image_edit_logic.py
@@ -0,0 +1,51 @@
+import os
+from core.workflow_assembler import WorkflowAssembler
+from core.workflow_utils import get_filename_prefix
+from core.utils import save_temp_image, handle_seed
+
+ASPECT_RATIO_PRESETS = {
+    "1:1 (Square)": (1024, 1024), 
+    "16:9 (Landscape)": (1344, 768), 
+    "9:16 (Portrait)": (768, 1344),
+    "4:3 (Classic)": (1152, 896), 
+    "3:4 (Classic Portrait)": (896, 1152),
+    "3:2 (Photography)": (1216, 832),
+    "2:3 (Photography Portrait)": (832, 1216)
+}
+
+WORKFLOW_RECIPE_PATH = "firered_image_edit_recipe.yaml"
+
+def process_inputs(ui_values, seed_override=None):
+    local_params = ui_values.copy()
+    
+    main_img = local_params.get('input_image')
+    if main_img is None:
+        raise ValueError("Please upload a main Input Image to edit.")
+
+    all_images = [main_img]
+    num_refs = int(local_params.get('ref_count_state', 0))
+    ref_images_list = local_params.get('ref_image_inputs', [])
+    
+    for i in range(num_refs):
+        if i < len(ref_images_list) and ref_images_list[i] is not None:
+            all_images.append(ref_images_list[i])
+    
+    image_filenames = [save_temp_image(img) for i, img in enumerate(all_images)]
+    local_params['image_stitch_chain'] = image_filenames
+    local_params['input_image'] = None
+
+    selected_ratio = local_params.get('aspect_ratio', "1:1 (Square)")
+    width, height = ASPECT_RATIO_PRESETS.get(selected_ratio, (1024, 1024))
+    local_params['width'] = width
+    local_params['height'] = height
+
+    seed = seed_override if seed_override is not None else int(local_params.get('seed', -1))
+    local_params['seed'] = handle_seed(seed)
+
+    local_params['filename_prefix'] = get_filename_prefix()
+
+    module_path = os.path.dirname(os.path.abspath(__file__))
+    assembler = WorkflowAssembler(WORKFLOW_RECIPE_PATH, base_path=module_path)
+    final_workflow = assembler.assemble(local_params)
+    
+    return final_workflow, None
